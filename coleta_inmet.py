37b9599fd8bffdc133fac7d51bd9a52c3919ff73
ricardogorayeb <ricardogorayeb@gmail.com>
2020-09-14 12:37:44 -0400

    update dados EMS

--- coleta_inmet.py
+++ coleta_inmet.py
@@ -7,16 +7,22 @@ def convert(lst):
 
 def retorna_dados_estacao():
     response = requests.get('https://api-redemet.decea.gov.br/aerodromos/info?api_key=v12b8I5cjvVF5d3d73Jlw9R30BeNMWD0rh4kT4OZ&localidade=SBEG&datahora=2020091100')
+    response2 = requests.get('https://api-redemet.decea.gov.br/mensagens/meteograma/SBEG?api_key=v12b8I5cjvVF5d3d73Jlw9R30BeNMWD0rh4kT4OZ&data_hora=2020090914&horas=00')
     #print(response.status_code)
     #print(response.__dict__)
     print(response.json())
-    #print(type(response.json()))
+    print(response2.json())
     dados_estacao = response.json()
     dados_estacao = dados_estacao['data']
-    print(type(dados_estacao))
-    print(dados_estacao['temperatura'])
-    print(dados_estacao['ur'])
-    #print(dados_cep['complemento'])
+    dados_estacao2 = response2.json()
+    dados_estacao2 = dados_estacao2['data']
+    #print(type(dados_estacao))
+    print('Temperatura em Eduardo Gomes: {}'.format(dados_estacao['temperatura']))
+    print('Umidade Relativa em Eduardo Gomes: {}'.format(dados_estacao['ur']))
+    print('Direcao do Vento em Eduardo Gomes: {}'.format(dados_estacao2['vnt_dir']))
+    print('Velocidade do Vento em Eduardo Gomes: {}'.format(dados_estacao2['vnt_vel']))
+    print('Direcao do Vento em Eduardo Gomes: {}'.format(dados_estacao2['vnt_dir']))
+    print('Pressao Atmosferica em Eduardo Gomes: {}'.format(dados_estacao2['qnh']))
     #return(dados_estacao)
 
 def retorna_dados_pokemon(pokemon):
